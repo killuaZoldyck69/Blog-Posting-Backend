9ef7050fc47374d534ff76257341168d9a7cc23e
killuaZoldyck69 <tanver694225@gmail.com>
2026-01-04 05:14:27 +0600

    add new fields role, phone and status in user table and setup basic email verification funciton

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,25 @@ import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.routes";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-
+import cors from "cors";
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:4000/",
+    credentials: true,
+
+    // // 3. methods: Standard REST methods
+    // methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+
+    // // 4. allowedHeaders: Headers Better Auth needs
+    // allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  })
+);
+
 app.use(express.json());
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, string } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
@@ -7,7 +7,34 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log("*********** Verification email send! ************");
+    },
   },
 });
