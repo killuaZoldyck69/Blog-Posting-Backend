5d790f4619da7f0b4c23a7f7a0132761b71a2f06
killuaZoldyck69 <tanver694225@gmail.com>
2026-01-03 17:05:14 +0600

    add createPost service logic

--- src/modules/post/post.controllers.ts
+++ src/modules/post/post.controllers.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
+import { postServices } from "./post.services";
 
 const createPost = async (req: Request, res: Response) => {
-  console.log(req.body);
-
-  res.send(req.body);
+  try {
+    const result = await postServices.createPost(req.body);
+    res.status(201).json({
+      message: "Post Created Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).send({
+      error: "Post Creation Failed",
+      details: error,
+    });
+  }
 };
 
 export const postControllers = {
