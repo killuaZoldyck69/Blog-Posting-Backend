79fc400750bac59a6213146f27fccdf75a346e56
killuaZoldyck69 <tanver694225@gmail.com>
2026-01-03 19:02:01 +0600

    install betterAuth for user authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.routes";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
